246bebdd9f64e00d7ab4310063f4cae61f333749
jianjun1.wang <jianjun1.wang@zhangmen.com>
2021-02-09 11:22:41 +0800

    init

---
+++ me-base/src/main/java/com/me/mebase/MeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.me.mebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeBaseApplication.class, args);
+    }
+
+}
